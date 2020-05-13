7dbacb36d056705c0ee99cb5b221c5f59ab5d912
PranjalVerma06 <pranjalverma@gmail.com>
2020-05-13 20:53:01 +0530

    siteLiveOnNetlify

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,8 +26,12 @@ $(document).ready(function () {
 
 $("#contactForm").submit(function (e) {
   e.preventDefault();
-});
 
+  var $form = $(this);
+  $.post($form.attr("action"), $form.serialize()).then(function () {
+    alert("Thank you! We will repond you soon");
+  });
+});
 // ==========================
 // Defining Functions
 // ==========================
